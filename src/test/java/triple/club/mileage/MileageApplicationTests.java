80e3b896162c5387f83e637b7fc321e50cbc14fd
yunhwan <pyh8618@gmail.com>
2022-07-07 21:04:13 +0900

    ADD: 프로젝트 init 파일 추가

---
+++ src/test/java/triple/club/mileage/MileageApplicationTests.java
@@ -0,0 +1,13 @@
+package triple.club.mileage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MileageApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
