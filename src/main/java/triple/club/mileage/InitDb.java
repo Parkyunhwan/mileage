13ad6ee937ede32f1146ac22c5b44206d6755474
yunhwan <pyh8618@gmail.com>
2022-07-11 02:25:55 +0900

    CHORE: 초기 데이터 테스트하기 쉽게 변경

--- src/main/java/triple/club/mileage/InitDb.java
+++ src/main/java/triple/club/mileage/InitDb.java
@@ -28,15 +28,18 @@ public class InitDb {
     static class InitService {
 
         private final EntityManager em;
-        private static final String PLACE_ID1 = "2e4baf1c-5acb-4efb-a1af-eddada31b00f";
-        private static final String USER_ID1 = "3ede0ef2-92b7-4817-a5f3-0c575361f745";
-
+        private static final String PLACE_ID1 = "1";
+        private static final String USER_ID1 = "1";
+        private static final String USER_ID2 = "2";
         public void dbInit1() {
             Place place = createPlace(PLACE_ID1, "첫번째장소");
             em.persist(place);
 
             User user = createUser(USER_ID1, "USER1");
             em.persist(user);
+
+            User user2 = createUser(USER_ID2, "USER1");
+            em.persist(user2);
         }
 
         private Place createPlace(String placeId, String name) {
