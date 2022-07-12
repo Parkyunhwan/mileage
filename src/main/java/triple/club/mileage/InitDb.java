7c3b1cea00cc1a4b9b4feeadde60846759748b4f
yunhwan <pyh8618@gmail.com>
2022-07-12 23:54:36 +0900

    ADD: 초기데이터 추가

--- src/main/java/triple/club/mileage/InitDb.java
+++ src/main/java/triple/club/mileage/InitDb.java
@@ -28,17 +28,25 @@ public class InitDb {
 
         private final EntityManager em;
         private static final String PLACE_ID1 = "2e4baf1c-5acb-4efb-a1af-eddada31b00f";
+        private static final String PLACE_ID2 = "2e4baf1c-5acb-4efb-a1af-eddada31b00e";
         private static final String USER_ID1 = "3ede0ef2-92b7-4817-a5f3-0c575361f745";
-        private static final String USER_ID2 = "3ede0ef2-92b7-4817-a5f3-0c575361f746";
+        private static final String USER_ID2 = "ktde0ef2-92b7-4817-a5f3-0c575361f746";
+        private static final String USER_ID3 = "acde0ef2-92b7-4817-a5f3-0c575361f746";
 
         public void dbInit1() {
             Place place = createPlace(PLACE_ID1, "첫번째장소");
             em.persist(place);
 
+            Place place2 = createPlace(PLACE_ID2, "두번째장소");
+            em.persist(place2);
+
             User user = createUser(USER_ID1, "USER1");
             em.persist(user);
 
             User user2 = createUser(USER_ID2, "USER2");
+            em.persist(user2);
+
+            User user3 = createUser(USER_ID3, "USER3");
             em.persist(user2);
         }
 
